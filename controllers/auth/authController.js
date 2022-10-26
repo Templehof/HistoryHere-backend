378a5275fe92da1f05510ccfe0e149a3b86652d7
Ivan Riumkin <ivri0001@gmail.com>
2022-10-26 16:23:07 +0200

    cookie sending functionality

--- controllers/auth/authController.js
+++ controllers/auth/authController.js
@@ -10,8 +10,24 @@ const signToken = (id) =>
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 
+const cookieOptions = {
+  expires: new Date(
+    Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+  ),
+  httpOnly: true,
+};
+
+if(process.env.NODE_ENV === "production"){
+  cookieOptions.secure = true;
+}
+
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+
+  res.cookie("jwt", token, cookieOptions);
+
+  //remove the password from the output
+  user.passwrod = undefined
 
   res.status(statusCode).json({
     status: "success",
